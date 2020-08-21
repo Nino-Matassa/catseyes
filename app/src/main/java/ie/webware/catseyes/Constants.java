890517d71b17acab7413ee8b30436f73a9b54f79
Nino Matassa <nino.matassa@gmail.com>
2020-08-21 09:42:20 +0100

    Ongoing

--- app/src/main/java/ie/webware/catseyes/Constants.java
+++ app/src/main/java/ie/webware/catseyes/Constants.java
@@ -4,10 +4,14 @@ public final class Constants
 {
 	private Constants() {}
 	public static final String worldOmeterURL = "https://covid.ourworldindata.org/data/owid-covid-data.json";
-		public static final String jsonExpression = "[^a-zA-Z0-9.\\[\\]\\{\\}]";
+		public static final String jsonWood = "[^a-zA-Z0-9.\\[\\]\\{\\}]";
+		public static final String jsonTree = "[^a-zA-Z0-9.]";
 		public static final String dbName = ":memory:";
 		public static final String tblRegion = "Region";
 		public static final String tblCountry = "Country";
 		public static final String tblData = "Data";
+		public static final String colCountry = "Country";
+		//public static final String jsonObjectOpen = "{";
+		//public static final String jsonArrayOpen = ":[";
 }
 
--- app/src/main/java/ie/webware/catseyes/WorldOmeterDatabase.java
+++ app/src/main/java/ie/webware/catseyes/WorldOmeterDatabase.java
@@ -10,7 +10,8 @@ import java.util.*;
 class WorldOmeterDatabase {
 	
 		public WorldOmeterDatabase() {}
-
+			ArrayList<String> jsonFragment = new ArrayList<String>();
+			ArrayList<TableKeyValue> tableKeyValue = new ArrayList<TableKeyValue>();
 			public Void populateLocalDatabase() {
 				BufferedReader bufferedReader = null;
 				try {
@@ -19,21 +20,82 @@ class WorldOmeterDatabase {
 						InputStream inputStream =  httpUrlConnection.getInputStream();
 						bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 						
-						ArrayList<String> jsonFragment = new ArrayList<String>();
 						String line = null;
-						String value = null;
-						
+						int fragmentIndex = 0;
 						while((line = bufferedReader.readLine()) != null) {
-							jsonFragment.add(line);
-							// Populate database...
-							value = line.replaceAll(Constants.jsonExpression," ");
+							jsonFragment.add(line.replaceAll(Constants.jsonWood," "));
+							fragmentIndex++;
+							if(fragmentIndex == 1000) break; // test line
 						}
 						bufferedReader.close();
-						
 					} catch (Exception e) {
 						String err = e.toString();
 					}
+					// Speed read json
+					int level = 0;// 1 = Region, 2 = Country, 3 = Data
+					for(int i = 0; i < jsonFragment.size(); i++){
+						String line = jsonFragment.get(i);
+							if(line.equals("{") || line.equals("}") || line.equals("},")) {
+							if(line.contains("{")){
+								level++;	
+								continue;
+							}
+							else if(line.contains("}")){
+								level--;
+								continue;
+							}
+								
+							else if(line.contains("[") || line.contains("]"))
+								continue;
+						}
+						String table = null;
+						String key = null;
+						String value = null;
+						if(level == 1 && line.contains("{")) {
+							table = Constants.tblRegion;
+							line = line.replaceAll(Constants.jsonTree, "");
+							key = Constants.colCountry;
+							value = line;
+							level++;
+						}
+						else if(level == 2){
+							table = Constants.tblCountry;
+							String[] keyValue = line.split(" ");
+							String s = fixStringArray(keyValue); // fix s...
+							keyValue = s.split("+");
+							key = keyValue[0].toString();
+							value = keyValue[1].toString();
+						}
+						else if (level == 3) {
+							table = Constants.tblData;
+							String[] keyValue = line.split(" ");
+							key = keyValue[0].toString();
+							value = keyValue[1].toString();
+						}
+						TableKeyValue tkv = new TableKeyValue();
+						tkv.table = table;
+						tkv.key = key;
+						tkv.value = value;
+						tableKeyValue.add(tkv);
+					}
 				return null;
+			}
+			
+		private String fixStringArray(String[] keyValue){
+				String key = null;
+				String value = "";
+				boolean isKey = true;
+				for(int i = 0; i < keyValue.length; i++) {
+					if(keyValue[i].length() > 0 && isKey) {
+						key = keyValue[i];
+						isKey = false;
+					} else {
+						if(keyValue[i].length() > 0)
+							value += keyValue[i] + " ";
+					}
+				}
+				//value = value.replace(value.substring(value.length()-1), "");
+				return key + "+" + value;
 			}
 			
 			private class TableKeyValue {
