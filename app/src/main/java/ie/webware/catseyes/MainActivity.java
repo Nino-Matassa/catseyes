253f7087e16c8439376601f38f7156930fb047ca
Nino Matassa <nino.matassa@gmail.com>
2020-09-05 09:30:40 +0100

    list view start

--- app/src/main/java/ie/webware/catseyes/MainActivity.java
+++ app/src/main/java/ie/webware/catseyes/MainActivity.java
@@ -12,34 +12,96 @@ import android.database.*;
 
 public class MainActivity extends Activity 
  {
+  SQLiteDatabase db = Database.getTestInstance();
+
+  ExpandableListAdapter listAdapter;
+  ExpandableListView expListView;
+  List<String> lstParent;
+  HashMap<String, List<String>> lstChild;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
-    playWithDatabase();
+    //playWithDatabase();
+    //buildDatabase();
+
+    // get the listview
+    expListView = (ExpandableListView) findViewById(R.id.lvParent);
+
+    // preparing list data
+    prepareListData();
+
+    listAdapter = new ExpandableListAdapter(this, lstParent, lstChild);
+
+    // setting list adapter
+    expListView.setAdapter(listAdapter);
    }
-   
+
+  /*
+   * Preparing the list data
+   */
+  private void prepareListData() {
+    lstParent = new ArrayList<String>();
+    lstChild = new HashMap<String, List<String>>();
+
+    Cursor cRegion = db.rawQuery("select continent from region", null);
+     {
+      try {       
+        long l = cRegion.getCount();
+        cRegion.moveToFirst(); // add region to parent
+        for(int I = 0; I < cRegion.getCount(); I++) {
+          String continent = cRegion.getString(cRegion.getColumnIndex("Continent"));
+          lstParent.add(continent);
+          cRegion.moveToNext();
+         }
+
+         { // add country to region
+          for(int o = 0; o < lstParent.size(); o++) {
+            String continent = lstParent.get(o).toString();
+            Cursor cCountry =  
+             db.rawQuery("select location from country join region on country.fk_region = region.id where region.continent = '" + 
+                         continent + "' order by location asc", null);
+            List<String> lstCountry = new ArrayList<String>();
+            String country = null;
+            cCountry.moveToFirst();
+            for(int i = 0; i < cCountry.getCount(); i++) {
+              country = cCountry.getString(cCountry.getColumnIndex("location"));
+              lstCountry.add(country);
+              cCountry.moveToNext();
+             }
+            lstChild.put(lstParent.get(o), lstCountry);
+          }
+         }
+       } catch(Exception e) {
+        Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+       } finally {
+        //db.close();    
+       }
+     }
+   }
+
   public void playWithDatabase() throws SQLiteFullException {
     SQLiteDatabase db = Database.getTestInstance();
     String continent = null;
-   try {
-     Cursor c = db.rawQuery("select continent from region", null);
-     long l = c.getCount();
-     c.moveToFirst();
-     for(int i = 0; i < c.getCount() && c.getCount() > 0; i++) {
-      continent = c.getString(c.getColumnIndex("Continent"));
-      c.moveToNext();
+    try {
+      Cursor c = db.rawQuery("select continent from region", null);
+      long l = c.getCount();
+      c.moveToFirst();
+      for(int i = 0; i < c.getCount() && c.getCount() > 0; i++) {
+        continent = c.getString(c.getColumnIndex("Continent"));
+        c.moveToNext();
+       }
+
+     } catch(Exception e) {
+      Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
+     } finally {
+      db.close();    
      }
-     
-   } catch (Exception e) {
-     Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-   } finally {
-     db.close();    
    }
-  }
 
   public boolean buildDatabase() {
-   
+
     Thread thread = new Thread(new Runnable() {
        @Override 
        public void run() {
@@ -50,7 +112,7 @@ public class MainActivity extends Activity
           }
         }
       });
-    
+
     Toast.makeText(MainActivity.this, "Initialising Worldometer Data", Toast.LENGTH_LONG).show();
     thread.start();
     try {
