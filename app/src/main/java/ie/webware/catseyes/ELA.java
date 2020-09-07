0994ba054dddc92577da95ec63815661223041fa
Nino Matassa <nino.matassa@gmail.com>
2020-09-07 09:02:31 +0100

    ELA

--- app/src/main/java/ie/webware/catseyes/ELA.java
+++ app/src/main/java/ie/webware/catseyes/ELA.java
@@ -4,89 +4,48 @@ import android.graphics.*;
 import android.view.*;
 import android.widget.*;
 import java.util.*;
+import android.widget.ExpandableListView.*;
 
 public class ELA extends BaseExpandableListAdapter
  {
-
-  private Context _context;
-  private List<String> _listDataHeader; // header titles
-  // child data in format of header title, child title
-  private HashMap<String, List<String>> _listDataChild;
-
-  public ELA(Context context, List<String> listDataHeader,
-                               HashMap<String, List<String>> listChildData) {
-    this._context = context;
-    this._listDataHeader = listDataHeader;
-    this._listDataChild = listChildData;
-   }
-
-  @Override
-  public Object getChild(int groupPosition, int childPosititon) {
-    return this._listDataChild.get(this._listDataHeader.get(groupPosition))
-     .get(childPosititon);
-   }
-
-  @Override
-  public long getChildId(int groupPosition, int childPosition) {
-    return childPosition;
-   }
-
-  @Override
-  public View getChildView(int groupPosition, final int childPosition,
-                           boolean isLastChild, View convertView, ViewGroup parent) {
-
-    final String childText = (String) getChild(groupPosition, childPosition);
-
-    if (convertView == null) {
-      LayoutInflater infalInflater = (LayoutInflater) this._context
-       .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-      convertView = infalInflater.inflate(R.layout.list_item, null);
-     }
-
-    TextView txtListChild = (TextView) convertView
-     .findViewById(R.id.lblListItem);
-
-    txtListChild.setText(childText);
-    return convertView;
-   }
-
-  @Override
-  public int getChildrenCount(int groupPosition) {
-    return this._listDataChild.get(this._listDataHeader.get(groupPosition))
-     .size();
-   }
-
-  @Override
-  public Object getGroup(int groupPosition) {
-    return this._listDataHeader.get(groupPosition);
-   }
+  private Context context = null;
+  private List<String> lstParent = null;
+  private HashMap<String, List<String>> hLstChild = null;
+  
+  public ELA(Context _context, List<String> _lstParent, HashMap<String, List<String>> _hLstChild) {
+   context = _context;
+   lstParent = _lstParent;
+   hLstChild = _hLstChild;
+  }
 
   @Override
   public int getGroupCount() {
-    return this._listDataHeader.size();
+    return lstParent.size();
    }
 
   @Override
-  public long getGroupId(int groupPosition) {
-    return groupPosition;
+  public int getChildrenCount(int index) { // called for each group... use this method for database populating?
+    return hLstChild.get(lstParent.get(index)).size();
    }
 
   @Override
-  public View getGroupView(int groupPosition, boolean isExpanded,
-                           View convertView, ViewGroup parent) {
-    String headerTitle = (String) getGroup(groupPosition);
-    if (convertView == null) {
-      LayoutInflater infalInflater = (LayoutInflater) this._context
-       .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-      convertView = infalInflater.inflate(R.layout.list_group, null);
-     }
+  public Object getGroup(int index) {
+    return lstParent.get(index);
+   }
 
-    TextView lblListHeader = (TextView) convertView
-     .findViewById(R.id.lblListHeader);
-    lblListHeader.setTypeface(null, Typeface.BOLD);
-    lblListHeader.setText(headerTitle);
+  @Override
+  public Object getChild(int iGroup, int iChild) {
+    return this.hLstChild.get(this.lstParent.get(iGroup)).get(iChild);
+   }
 
-    return convertView;
+  @Override
+  public long getGroupId(int index) {
+    return index;
+   }
+
+  @Override
+  public long getChildId(int iGroup, int iChild) {
+    return iChild;
    }
 
   @Override
@@ -95,8 +54,36 @@ public class ELA extends BaseExpandableListAdapter
    }
 
   @Override
-  public boolean isChildSelectable(int groupPosition, int childPosition) {
+  public View getGroupView(int position, boolean isExpanded, View vPopulate, ViewGroup vGroup) {
+    // populate group view at position, called for each position
+    if(vPopulate == null) {
+     LayoutInflater lInflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+     vPopulate = lInflater.inflate(R.layout.list_group, null);
+    }
+    TextView vTxtParent = (TextView)vPopulate.findViewById(R.id.vTxtParent);
+    vTxtParent.setText((String)getGroup(position));
+    return vPopulate;
+   }
+
+  @Override
+  public View getChildView(int iParent, int iChild, boolean isLastChild, View vPopulate, ViewGroup vParent) {
+    // Called for each child in the hash list
+    if(vPopulate == null) {
+     LayoutInflater lInflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+     vPopulate = lInflater.inflate(R.layout.list_item, null);
+    }
+    TextView vTxtChild = (TextView)vPopulate.findViewById(R.id.vTxtChild);
+    vTxtChild.setText((String)getChild(iParent, iChild));
+    return vPopulate;
+   }
+
+  @Override
+  public boolean isChildSelectable(int iGroup, int iChild) {
     return true;
    }
-  
+   
+//  @Override
+//  public boolean OnChildClickListener(ExpandableListView parent, View v, int iGroup, int iChild, long id) {
+//   return true;
+//  }
 }
